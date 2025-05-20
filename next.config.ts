fc1f4a57f5474dfe9b5526205b33129c05fee59f
vishalbac10 <86062099+vishalbac10@users.noreply.github.com>
2025-05-21 00:49:44 +0530

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'cdn.shopify.com',
+      'wpastra.com',
+      'cdn.dribbble.com',
+      'placehold.co',
+      'via.placeholder.com'
+    ],
+  },
+};
+
+module.exports = nextConfig;
